5a6de1aab99260884c0b00f9fc2b8f8d50a5582a
Sylvère Grégeois <s.gregeois@mapado.com>
2015-03-03 17:48:59 +0100

    added methods getChargeList and getInvoiceList in CustomerProxy

--- src/Model/CustomerProxy.php
+++ src/Model/CustomerProxy.php
@@ -2,10 +2,51 @@
 
 namespace Mapado\Stripe\Model;
 
+use Mapado\Stripe\Model\ChargeProxy as ChargeProxy;
+use Mapado\Stripe\Model\InvoiceProxy as InvoiceProxy;
+
 class CustomerProxy extends StripeObject
 {
     public function __get($key)
     {
         return $this->stripeObject->{$key};
+    }
+
+    /**
+     * getChargeList
+     *
+     * @param array $params
+     * @access public
+     * @return array
+     */
+    public function getChargeList(array $params = array())
+    {
+        $stripeChargeList = \Stripe_Charge::all(['customer' => $this->getId()] + $params);
+        $chargeList = [];
+        if (!empty($stripeChargeList)) {
+            foreach ($stripeChargeList['data'] as $stripeCharge) {
+                $chargeList[] = new ChargeProxy($stripeCharge, $this);
+            }
+        }
+        return $chargeList;
+    }
+
+    /**
+     * getInvoiceList
+     *
+     * @param array $params
+     * @access public
+     * @return array
+     */
+    public function getInvoiceListForClient(array $params = array())
+    {
+        $stripeInvoiceList = \Stripe_Invoice::all(['customer' => $this->getId()] + $params);
+        $invoiceList = [];
+        if (!empty($stripeInvoiceList)) {
+            foreach ($stripeInvoiceList['data'] as $stripeInvoice) {
+                $invoiceList[] = new InvoiceProxy($stripeInvoice, $this);
+            }
+        }
+        return $invoiceList;
     }
 }
